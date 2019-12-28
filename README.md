717c5bada7859a7fc7d69ac4d6b43aed355eab0a
Camilo Metaute <camilometaute@lahaus.com>
2019-12-28 10:24:32 -0500

    JS baby steps :) kind of variables - naming variables

---
+++ README.md
@@ -0,0 +1,41 @@
+# learning-js
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn run serve
+```
+
+### Compiles and minifies for production
+```
+yarn run build
+```
+
+### Lints and fixes files
+```
+yarn run lint
+```
+
+
+Notas:
+
+Formas de escribir variables:
+//block scoped {}
+let age = 30; // Can be updated
+const sport = true; // Can't be updated
+
+//camel case
+const iLovePizza = true;
+
+//UpperCamelCase
+const ILoveToEatHotDogs = false;
+
+// snake case!!
+const this_is_snake_case = true;
+
+ESLint para evitar malas prácticas como console.log
+Prettier para dar buen formato al JS
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="He we will learn JS"/>
+    <button v-on:click="reverseMessage" type="button" name="button">Hello world</button>
   </div>
 </template>
 
@@ -12,6 +13,30 @@ export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  methods: {
+    reverseMessage: function () {
+
+      //function scoped
+      var firstVariable = 'This is the first variable'; //Can be updated
+
+      //block scoped {}
+      let age = 30; // Can be updated
+      const sport = true; // Can't be updated
+
+      //camel case
+      const iLovePizza = true;
+
+      //UpperCamelCase
+      const ILoveToEatHotDogs = false;
+
+      // snake case!!
+      const this_is_snake_case = true;
+
+      //kebab-case this is not allowed in javascrip
+
+      alert( firstVariable + age + sport + ILoveToEatHotDogs + this_is_snake_case + iLovePizza);
+    }
   }
 }
 </script>
