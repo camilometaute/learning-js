847c22fa2ef25fa813644e8283aadbe7c5a85490
Camilo Metaute <camilometaute@lahaus.com>
2020-04-10 18:02:00 -0500

    creating type effect element

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="He we will learn JS"/>
-    <button v-on:click="reverseMessage" type="button" name="button">Hello world</button>
+    <TypeAutomatic/>
+    <HelloWorld msg="Descarga este documento"/> <br>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TypeAutomatic from './components/TypeAutomatic.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  },
-  methods: {
-    reverseMessage: function () {
-
-      //function scoped
-      var firstVariable = 'This is the first variable'; //Can be updated
-
-      //block scoped {}
-      let age = 30; // Can be updated
-      const sport = true; // Can't be updated
-
-      //camel case
-      const iLovePizza = true;
-
-      //UpperCamelCase
-      const ILoveToEatHotDogs = false;
-
-      // snake case!!
-      const this_is_snake_case = true;
-
-      //kebab-case this is not allowed in javascrip
-
-      alert( firstVariable + age + sport + ILoveToEatHotDogs + this_is_snake_case + iLovePizza);
-    }
+    HelloWorld,
+    TypeAutomatic
   }
 }
 </script>
