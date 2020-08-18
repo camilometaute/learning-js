7dc27fc3ba549e2138db4ef7f4d65a3795d45646
Camilo Metaute <camilometaute@lahaus.com>
2020-08-17 19:34:37 -0500

    remove lines to avoid alert in code

--- src/components/TraningJsDom.vue
+++ src/components/TraningJsDom.vue
@@ -1,7 +1,61 @@
 <template>
   <div>
-    <h2>Bikes</h2>
-    
+    <div v-if="hiddenElement">
+      <h4>Element to toggle 1</h4>
+      <h5>Element to toggle 2</h5>
+
+    </div>
+    <button @click="hiddenElement = !hiddenElement">Toggle Event</button>
+    <span style="display: block">Training vue DOM
+    {{sayHello()}}</span>
+
+    <br>
+    <button v-on:click="counter--">-</button>
+    <button v-on:click="counter = 0">Reset</button>
+    <button v-on:click="counter++">+</button>
+    <p>{{counter}}</p>
+    <p>{{checkResult()}}</p>
+    <br>
+    <div
+      style="width: 100%; height: 30vh;background-color: #fafafa; display: flex; align-items: center; justify-content:center; flex-direction: column; font-size: 2.6rem;"
+      v-on:mousemove="updateCoordinates">
+      <p style="margin: 1rem 0 2rem 0;">Coordinates {{x}} {{y}}</p>
+      <span
+        style="background-color: pink;"
+        v-on:mousemove.stop="">
+        Stop here coordinates</span>
+        <!-- listening keyboard events -->
+    </div>
+    <input type="text" name="" placeholder="name" v-model="name">
+    <p>{{name}}</p>
+    <div class="colors-interactive">
+      <div
+        class="colors-interactive__box"
+        @click="toggleColorRed = !toggleColorRed"
+        :class="{'colors-interactive__red' : toggleColorRed}">
+      </div>
+
+      <div
+        class="colors-interactive__box"
+        @click="toggleColorBlue = !toggleColorBlue"
+        :class="InputColor"
+      >
+      </div>
+
+      <div
+        class="colors-interactive__box"
+        @click="toggleColorGreen = !toggleColorGreen"
+        :class="toggleClasses"
+      >
+      </div>
+      <div
+        class="colors-interactive__box"
+        :style="myStyle"
+      >
+      </div>
+    </div>
+    <input placeholder="Which color do you want" v-model="InputColor">
+    <input placeholder="Which width" v-model="width">
   </div>
 </template>
 
